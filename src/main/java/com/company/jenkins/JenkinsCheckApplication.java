f7a4298a941ac1c2f913f54d206984ca39e4346c
singhjaijeet106 <singhjaijeet106@gmail.com>
2025-07-15 13:27:57 -0400

    First commit

---
+++ src/main/java/com/company/jenkins/JenkinsCheckApplication.java
@@ -0,0 +1,14 @@
+package com.company.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCheckApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		SpringApplication.run(JenkinsCheckApplication.class, args);
+	}
+
+}
